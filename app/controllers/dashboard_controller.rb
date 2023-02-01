dd1b5c820d05eaa6814a6584157d58b8dc589ee5
hujuu <1961946+hujuu@users.noreply.github.com>
2023-02-01 10:44:39 +0900

    ユーザー情報を表示する機能

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,9 @@
+# ./app/controllers/dashboard_controller.rb
+class DashboardController < ApplicationController
+  include Secured
+
+  def show
+	# session[:userinfo] was saved earlier on Auth0Controller#callback
+	@user = session[:userinfo]
+  end
+end
